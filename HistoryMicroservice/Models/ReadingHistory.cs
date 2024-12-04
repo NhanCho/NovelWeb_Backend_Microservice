18982820487890278f497e497f2c529119f321d3
MrLeKiet <kietsvis@gmail.com>
2024-12-04 20:34:33 +0700

    Add History Microservice

---
+++ HistoryMicroservice/Models/ReadingHistory.cs
@@ -0,0 +1,12 @@
+﻿namespace HistoryMicroservice.Models
+{
+    public class ReadingHistory
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int NovelId { get; set; }
+        public int? LastReadChapterId { get; set; }
+        public DateTime LastReadDate { get; set; }
+    }
+
+}
