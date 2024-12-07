a73cdbc76b16b220dc1357b4bedd57e9e7ed1574
Huy Minh <minh.th12558@sinhvien.hoasen.edu.vn>
2024-12-07 22:14:42 +0700

    disable auto open browser, swagger

--- NovelMicroservice/Program.cs
+++ NovelMicroservice/Program.cs
@@ -7,33 +7,27 @@ using NovelMicroservice.Repositoriesl;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 33))));
+
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<INovelRepository, NovelRepository>();
-builder.Services.AddScoped<IChapterRepository, ChapterRepository>(); // Đăng ký ChapterRepository
-
+builder.Services.AddScoped<IChapterRepository, ChapterRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(); // Giao diện Swagger UI
-}
-// Configure the HTTP request pipeline.
+
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseHttpsRedirection();
 
